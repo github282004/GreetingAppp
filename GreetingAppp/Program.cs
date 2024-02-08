4eeb3630877b6da6dba434121d6b13d1f8e7877e
github282004 <leeplata03@gmail.com>
2024-02-08 08:36:15 +0800

    Add project files.

---
+++ GreetingAppp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GreetingAppp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Enter your Name");
+        string name = Console.ReadLine();
+        Console.WriteLine("Enter your total enrolled courses:");
+        int courses;
+        courses = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the price of your favorite book:");
+        double book;
+        book = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Name:" + " " + name);
+        Console.WriteLine("Total enrolled courses:" + " " + courses);
+        Console.WriteLine("Price of my favorite book" + " " + book);
+        }
+    }
+}
